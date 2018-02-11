aa22ee5f6b927c88e3861f1e96b6e626bd3f99e6
Edno Fedulo <edno.moraes@gmail.com>
2018-02-11 15:54:37 -0300

    Initial Commit

---
+++ udacity-mememe1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  udacity-mememe1
+//
+//  Created by Edno Fedulo on 11/02/18.
+//  Copyright © 2018 Fedulo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
